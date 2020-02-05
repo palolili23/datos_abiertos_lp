3b2c783565dd7c2e149fcfbaca44da920622522d
Paloma <palolili@gmail.com>
2020-02-05 09:40:59 +0100

    intento de iterar y unir todos los sets de partos para todos los anios

---
+++ partos_tiempo.R
@@ -0,0 +1,32 @@
+library(tidyverse)
+library(here)
+here()
+
+
+path <- paste0(here(), "/00_datos_crudos/partos/")
+
+files <- fs::dir_ls(path, regexp = "\\.csv$")
+
+files
+
+
+data_list <- files %>% 
+  map(read_csv2) 
+
+names_data <- names(data_list)
+
+names_data <- str_extract(names_data, "[2](.*)[^.csv]")
+
+names_data
+
+data_list <- Map(cbind, data_list, anio = names_data)
+
+data_list_columns <-
+  data_list %>% 
+  pluck(1) %>% 
+  colnames()
+
+data_list %>% map(data_list_columns, select)
+
+data_list %>% 
+  select(data_list_columns)
